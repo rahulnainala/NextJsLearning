f9c7d484538985976504611c91a5cff9ddaef0db
Rahul Nainala <rahulnainala2@gmail.com>
2024-11-27 19:43:01 +0530

    NextJs Learning fixes -01

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import { Poppins } from "next/font/google";
 import { Lusitana } from "next/font/google";
 
-export const poppins = Poppins({ weight: "500" });
-export const lusitana = Lusitana({ weight: "400" });
+export const poppins = Poppins({ weight: "500", subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
