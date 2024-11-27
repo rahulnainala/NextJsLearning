dff64e83fca06201f6c7e5d48cd9128539e5d6a6
Rahul Nainala <rahulnainala2@gmail.com>
2024-11-27 19:37:16 +0530

    NextJs Learning

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <p>This is a invoice Page</p>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Poppins } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+export const poppins = Poppins({ weight: "500" });
+export const lusitana = Lusitana({ weight: "400" });
